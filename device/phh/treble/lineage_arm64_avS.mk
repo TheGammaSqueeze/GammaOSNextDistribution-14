c1ae9b202dde0bb62e1530c1259cf66427852d6e
gamma <thegammasqueeze@gmail.com>
2026-03-02 19:58:43 +0000

    Rebrand PRODUCT_MODEL to GammaOS Next
    Rename "TrebleDroid with GApps" to "GammaOS Next Full" and
    "TrebleDroid vanilla" to "GammaOS Next Lite" across all treble
    device makefiles.

--- device/phh/treble/lineage_arm64_avS.mk
+++ device/phh/treble/lineage_arm64_avS.mk
@@ -9,7 +9,7 @@ PRODUCT_NAME := lineage_arm64_avS
 PRODUCT_DEVICE := tdgsi_arm64_a
 PRODUCT_BRAND := google
 PRODUCT_SYSTEM_BRAND := google
-PRODUCT_MODEL := TrebleDroid vanilla
+PRODUCT_MODEL := GammaOS Next Lite
 
 # Overwrite the inherited "emulator" characteristics
 PRODUCT_CHARACTERISTICS := device
